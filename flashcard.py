7f14cf62c1efdb5301e0bd65760785310fcf75bd
Jiuhong <jiuhong@casperlabs.io>
2023-11-09 15:12:28 +0800

    init

---
+++ flashcard.py
@@ -0,0 +1,24 @@
+import random
+import os
+with open("words.list.txt", "r") as f:
+    lines = f.readlines()
+    list = random.sample(range(0, len(lines)-1), len(lines)-1)
+
+    for index in list:
+        line = lines[index]
+        words = line.split("=>")
+        s = 0
+        while True:
+            print(words[s])
+            data = input("\n")
+            os.system('cls||clear')
+            if not data:
+                s = 1-s
+            else:
+                break
+
+
+# import random
+# data = list(range(numLow, numHigh))
+# random.shuffle(data)
+# print data
